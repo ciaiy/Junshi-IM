e4976003d1504406779ebd75729f7a2b2f5f5ebc
ciaiy <36753054+ciaiy@users.noreply.github.com>
2020-10-05 15:00:57 +0800

    Update config.hpp
    增加config.cpp的说明

--- common/config.hpp
+++ common/config.hpp
@@ -7,6 +7,7 @@ namespace im
 {
     namespace common
     {
+        // 后期替换掉这种写法
         static std::string redisConfig = "{\"ip\": \"127.0.0.1\",\"port\": \"6379\",\"username\": \"null\", \"password\": \"null\"}";
     } // namespace common
 } // namespace im
