e61e1a1b8396e8ac65a98df957266b9af098528c
ciaiy <1374390675@qq.com>
2020-10-18 08:57:26 +0800

    to #SCRUM4 增加Exception类

---
+++ common/Exception.cpp
@@ -0,0 +1,15 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#include "Exception.hpp"
+
+using namespace im::common;
+
+Exception::Exception(std::string msg)
+    : message_(std::move(msg)),
+      stack_(std::CurrentThread::stackTrace(/*demangle=*/false))
+{
+    
+}
